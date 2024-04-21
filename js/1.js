851dc51aadf880323044c7819584232f390c17c6
rasagnyaa <rasagnyakannuri@gmail.com>
2024-04-21 15:41:00 +0530

    commit

--- js/1.js
+++ js/1.js
@@ -1,12 +1,12 @@
 for(i=1;i<=15;i++){
     if(i%3==0 && i%5==0){
-        console.log("fizzbuzz")
+        console.log(i,"= fizzbuzz")
     }
     else if(i%3==0){
-console.log("fizz");
+console.log(i,"= fizz");
         }
         else if(i%5==0){
-            console.log("buzz")
+            console.log(i,"= buzz")
         }else{
             console.log(i)
         }
