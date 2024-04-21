be77b7bcdc239a4dcc3783b165018ecb52fd412e
rasagnyaa <rasagnyakannuri@gmail.com>
2024-04-21 22:20:32 +0530

    commit

---
+++ js/w3/p1.js
@@ -0,0 +1,14 @@
+let num1 = 3;
+let num2 = -7;
+let num3 = 2;
+
+let product = num1 * num2 * num3;
+
+if (product > 0) {
+alert("The sign of the product is positive.");
+} else if (product < 0) {
+alert("The sign of the product is negative.");
+} else {
+alert("The product is zero.");
+}
+console.log(product)
---
+++ js/w3/p2.js
@@ -0,0 +1,9 @@
+num1=10
+num2=12
+if(num1>num2){
+    console.log(num1," is greater num2" ,num2);
+}else if(num2>num1){
+    console.log(num2,"is greater than", num1);
+}else{
+    console.log(nothing)
+}
