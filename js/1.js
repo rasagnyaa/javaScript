e8594bb4cc3f48fc40ae64412331acba8d81d1ce
rasagnyaa <rasagnyakannuri@gmail.com>
2024-04-21 15:30:06 +0530

    task-1

--- js/1.js
+++ js/1.js
@@ -1,4 +1,18 @@
-function fun(){
-    return "rasagnya"
-}
-console.log(fun())
+for(i=1;i<=15;i++){
+    if(i%3==0 && i%5==0){
+        console.log("fizzbuzz")
+    }
+    else if(i%3==0){
+console.log("fizz");
+        }
+        else if(i%5==0){
+            console.log("buzz")
+        }else{
+            console.log(i)
+        }
+    }
+
+
+
+
+
