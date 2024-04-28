d9b4fab8ea8be48c691be4ec8b80ece49b813dff
rasagnyaa <rasagnyakannuri@gmail.com>
2024-04-28 11:26:22 +0530

    commit

--- strings/prb1.js
+++ strings/prb1.js
@@ -1,20 +1,26 @@
-debugger
+// debugger
+// function isPalindrome(str){
+// let str1=""
+// for(i=str.length-1;i>=0;i--){
+//   str1+=str[i]
+// }
+// // console.log(str1);
+// if(str==str1){
+//   return true;
+// }
+// else{
+//  return false
+// }
+// }
+// let str2=prompt("enter a string:")
+// if(isPalindrome(str2)==true)
+// console.log(`${str2} is Palindrome`);
+// else
+// console.log(`${str2} is not a Palindrome`);
+debugger;
 function isPalindrome(str){
-let str1=""
-for(i=str.length-1;i>=0;i--){
-  str1+=str[i]
+  return str.toLowerCase() === str.toLowerCase().split('').reverse().join('')
 }
-// console.log(str1);
-if(str==str1){
-  return true;
-}
-else{
- return false
-}
-}
-let str2=prompt("Enter a string: ");
-if(isPalindrome(str2)==true)
-console.log(`${str2} is Palindrome`);
-else
-console.log(`${str2} is not a Palindrome`);
-
+console.log(isPalindrome("MOM"))
+console.log(isPalindrome("ram"))
+console.log(isPalindrome("siri"))
