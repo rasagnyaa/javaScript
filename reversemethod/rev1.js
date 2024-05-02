838d56076285d07c4ad16071769cc82b784c315b
rasagnyaa <rasagnyakannuri@gmail.com>
2024-05-02 21:45:15 +0530

    reversemethod

---
+++ reversemethod/rev1.js
@@ -0,0 +1,3 @@
+let fruits=["apple","banana","orange"]
+let fruit=fruits.reverse()
+console.log("reverse of fruits [apple,banana,orange] =>",fruit)
---
+++ reversemethod/rev2.js
@@ -0,0 +1,3 @@
+let numbers=[5,10,15,20,25]
+let num=numbers.reverse()
+console.log("reverse of [5,10,15,20,25] => ",num)
