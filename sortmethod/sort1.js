a23d50a1481335756ce1126d2abc5f628a440a74
rasagnyaa <rasagnyakannuri@gmail.com>
2024-05-02 21:52:21 +0530

    sortmethod

---
+++ sortmethod/sort1.js
@@ -0,0 +1,5 @@
+let num=[4,5,2,7,1,3]
+let n=num.sort((a,b)=>{
+    return a-b
+})
+console.log("[4,5,2,7,1,3] => ",n)
---
+++ sortmethod/sort2.js
@@ -0,0 +1,5 @@
+let num=[60,50,40,30]
+let n=num.sort((b,a)=>{
+    return b-a
+})
+console.log("[20,30,40,50] => ",n)
